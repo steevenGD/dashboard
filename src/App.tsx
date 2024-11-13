291e1071e4aa2ad0f51ff1b4d476e16931a559a4
unknown <sjgomez@espol.edu.ec>
2024-11-13 14:26:22 -0500

    third

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 import Grid from '@mui/material/Grid2' 
 
 function App() {
-  const [count, setCount] = useState(0)
+//  const [count, setCount] = useState(0)
 
   return (
     <Grid container spacing={5}>
