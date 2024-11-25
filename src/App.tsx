617734508fb5643d285c1e26400191568f30d5f0
Ariana Gonzabay <arigalda@espol.edu.ec>
2024-11-25 13:40:33 -0500

    fourth

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
+import IndicatorWeather from './components/IndicatorWeather';
 import './App.css'
 import Grid from '@mui/material/Grid2' 
 
@@ -11,10 +12,10 @@ function App() {
     <Grid container spacing={5}>
 
         {/* Indicadores */}
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather /> </Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather /> </Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather /> </Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather /> </Grid>
 
         {/* Tabla */}
         <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
